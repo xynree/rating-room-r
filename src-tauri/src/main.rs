ec79d6469ad31695698fcae3ea6aad0bd274c03e
Zane Schaffer <personal.zane@gmail.com>
2023-01-26 23:18:02 -0800

    setup db path & add basic inset function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,10 @@ use serde::{Deserialize, Serialize};
 use sled_extensions::json::JsonEncoding;
 use sled_extensions::structured::Tree;
 use sled_extensions::DbExt;
+use tauri::{
+    api::path::{app_data_dir, BaseDirectory},
+    Manager, PathResolver,
+};
 
 #[derive(Deserialize, Serialize)]
 struct Rating {
@@ -21,35 +25,40 @@ struct RatingState {
 }
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(state: tauri::State<RatingState>) -> String {
-    if let Some(rating) = state.ratings.get("Takis").unwrap() {
+fn get(state: tauri::State<RatingState>, key: String) -> String {
+    if let Some(rating) = state.ratings.get(key).unwrap() {
         format!("{}'s cost {}", rating.name, rating.cost)
     } else {
         String::new()
     }
 }
 
+#[tauri::command]
+fn insert(state: tauri::State<RatingState>, rating: Rating) {
+    state
+        .ratings
+        .insert(rating.name.clone().as_str(), rating)
+        .unwrap();
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let db = sled_extensions::Config::default()
-        .path("db")
-        .open()
-        .expect("error while opening sled database");
-
-    let ratings = db
-        .open_json_tree("ratings")
-        .expect("error while opening ratings tree");
-
-    ratings.insert(
-        "Takis",
-        Rating {
-            name: "Takis".to_owned(),
-            cost: "2.0".to_owned(),
-        },
-    )?;
-
     tauri::Builder::default()
-        .manage(RatingState { ratings })
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let data_dir = app
+                .path_resolver()
+                .app_data_dir()
+                .expect("failed to find Data Dir");
+
+            let db = sled_extensions::Config::default()
+                .path(data_dir.join("db"))
+                .open()?;
+
+            let ratings = db.open_json_tree("ratings")?;
+
+            app.manage(RatingState { ratings });
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![get, insert])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
     Ok(())
--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,19 +1,36 @@
 <script lang="ts">
     import { invoke } from "@tauri-apps/api/tauri";
 
-    let name = "";
-    let greetMsg = "";
+    let response = "";
+    let rating_key = "";
 
-    async function greet() {
+    let new_rating = {
+        name: "",
+        cost: "",
+    };
+
+    async function get() {
         // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-        greetMsg = await invoke("greet");
+        response = await invoke("get", { key: rating_key });
     }
 
-    greet();
+    async function insert() {
+        await invoke("insert", { rating: new_rating });
+        response = `Inserted ${new_rating.name}`;
+    }
 </script>
 
 <div>
-    <div>
-        <p>{greetMsg}</p>
+    <div class="m-5">
+        <h1>Insert Rating</h1>
+        <input placeholder="Name..." bind:value={new_rating.name} />
+        <input placeholder="Cost..." bind:value={new_rating.cost} />
+        <button on:click={insert}>Insert</button>
+    </div>
+    <div class="m-5">
+        <h1>Get Rating</h1>
+        <input placeholder="Rating Name..." bind:value={rating_key} />
+        <button on:click={get}>Get</button>
+        <p>{response}</p>
     </div>
 </div>
