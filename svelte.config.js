8cbb19189befdb0678f4d7549c554b83a3a19206
Zane Schaffer <znschaffer@gmail.com>
2023-02-24 19:16:34 -0800

    fix prerender builds

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,16 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess()],
-  kit: {
-    alias: {
-      service: "src/service",
-    },
-    adapter: staticAdapter(),
-  },
+	preprocess: [vitePreprocess()],
+	kit: {
+		alias: {
+			service: "src/service",
+		},
+		adapter: staticAdapter(),
+		prerender: {
+			entries: ["*", "/items/[id]", "/items/[id]/edit"],
+		},
+	},
 };
 
 export default config;
