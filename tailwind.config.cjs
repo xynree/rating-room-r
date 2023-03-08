516a8f86504a7c3d12531329218683ee0f464da8
Zane Schaffer <znschaffer@gmail.com>
2023-03-07 16:18:26 -0800

    add ibm plex sans font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['IBM Plex Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-};
+}
