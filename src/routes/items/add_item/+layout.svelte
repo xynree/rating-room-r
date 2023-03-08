56c923b3672910e816357e7a9dab7b55e7ab87d9
Zane Schaffer <znschaffer@gmail.com>
2023-03-08 11:22:05 -0800

    add navbar to add item page

---
+++ src/routes/items/add_item/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { itemsStore } from 'store'
+</script>
+
+<nav class="flex justify-between p-6 w-screen text-sm">
+  <a href="/">my collection ({$itemsStore.items.length} items)</a>
+</nav>
+
+<slot />
+
+<style lang="postcss">
+  a {
+    @apply hover:text-gray-800 transition-all text-sm;
+  }
+</style>
